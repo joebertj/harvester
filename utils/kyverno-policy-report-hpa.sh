be3cd0cf0b73977885155fcf7a695fb29f4d32ce
Joebert Jacaba <joebertj@mac.home>
2026-03-27 07:28:04 +0100

    adding a script to show policies for hpa

---
+++ utils/kyverno-policy-report-hpa.sh
@@ -0,0 +1,11 @@
+#kubectl rollout restart deployment fastapi-metrics-app -n harvester-autoscaling-sim
+kubectl rollout restart deployment kyverno -n kyverno
+kubectl get policyreport -n harvester-autoscaling-sim -o json | python3 -c "
+import json, sys
+data = json.load(sys.stdin)
+for r in data.get('items', []):
+    for res in r.get('results', []):
+        if res.get('result') == 'fail':
+            name = res['resources'][0]['name'] if res.get('resources') else '?'
+            print(f\"{res['policy']:35s} {res['resources'][0]['kind']}/{name}\")
+" | sort -u
